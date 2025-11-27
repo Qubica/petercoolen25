fc80a4479e49b8658c2f26c9a125a5dffa78846e
Peter Coolen <info@petercoolen.com>
2025-11-27 13:04:18 +0100

    small tweaks

---
+++ src/app/sitemap.ts
@@ -0,0 +1,24 @@
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://www.petercoolen.com",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: "https://www.petercoolen.com/bespoke-shopify-solutions",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.petercoolen.com/one-brand-one-system",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+  ];
+}
